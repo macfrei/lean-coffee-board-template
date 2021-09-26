0974eab5848ab868009b74125dcd748281610713
Marie-Carleen Frei <lene@7freiheiten.de>
2021-09-26 12:16:43 +0200

    Add nanoid and write post request handler

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const { nanoid } = require('nanoid')
 const app = express()
 const port = 3000
 
-const cards = [{ title: 'What is HTML?', author: 'John Doe' }]
+const cards = [{ title: 'What is HTML?', author: 'John Doe', id: '123abc' }]
 
 app.use(express.json())
 
 app.get('/api/cards', (req, res) => {
   res.json(cards)
+})
+
+app.post('/api/cards', (req, res) => {
+  const id = nanoid()
+  const newCard = { ...req.body, id }
+
+  cards.push(newCard)
+  res.json(newCard)
 })
 
 app.listen(port, () => {
