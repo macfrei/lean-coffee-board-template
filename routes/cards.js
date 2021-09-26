4b893e6c85fd6bf31492ff109f817ed1fbca6309
Marie-Carleen Frei <lene@7freiheiten.de>
2021-09-26 13:12:23 +0200

    Implement error handling

--- routes/cards.js
+++ routes/cards.js
@@ -4,11 +4,11 @@ const router = express.Router()
 
 let cards = [{ title: 'What is HTML?', author: 'John Doe', id: '123abc' }]
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   res.status(200).json(cards)
 })
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   const id = nanoid()
   const newCard = { ...req.body, id }
   cards.push(newCard)
@@ -16,21 +16,41 @@ router.post('/', (req, res) => {
   res.status(200).json(newCard)
 })
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', (req, res, next) => {
   const { id } = req.params
-  const index = cards.findIndex(card => id === card.id)
+  const card = cards.find(card => card.id === id)
 
-  const newCard = { ...cards[index], ...req.body }
-  cards = [...cards.slice(0, index), newCard, ...cards.slice(index + 1)]
+  if (card) {
+    const index = cards.findIndex(card => id === card.id)
 
-  res.status(200).send(newCard)
+    const newCard = { ...cards[index], ...req.body }
+    cards = [...cards.slice(0, index), newCard, ...cards.slice(index + 1)]
+
+    res.status(200).send(newCard)
+  } else {
+    next({
+      statusCode: 404,
+      detail: `Could not find question with id: ${id}`,
+      message: 'Document not found',
+    })
+  }
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
   const { id } = req.params
-  cards = cards.filter(card => card.id !== id)
+  const card = cards.find(card => card.id === id)
 
-  res.status(200).send(`Card with id: ${id} was deleted.`)
+  if (card) {
+    cards = cards.filter(card => card.id !== id)
+
+    res.status(200).send(`Card with id: ${id} was deleted.`)
+  } else {
+    next({
+      statusCode: 404,
+      detail: `Could not find question with id: ${id}`,
+      message: 'Document not found',
+    })
+  }
 })
 
 module.exports = router
