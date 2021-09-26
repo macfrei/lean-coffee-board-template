18f1df4061dc6771baf5f17c1766f533b1fc4b0a
Marie-Carleen Frei <lene@7freiheiten.de>
2021-09-26 12:11:30 +0200

    Write simple server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello world!')
+const express = require('express')
+const app = express()
+const port = 3000
+
+const cards = [{ title: 'What is HTML?', author: 'John Doe' }]
+
+app.use(express.json())
+
+app.get('/api/cards', (req, res) => {
+  res.json(cards)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
