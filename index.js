da5b449223faef4155090bc668c61e0af696a44e
Marie-Carleen Frei <lene@7freiheiten.de>
2021-09-26 13:06:53 +0200

    Add error route

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use(express.json())
 
 app.use('/api/cards', require('./routes/cards'))
 
+app.use(require('./routes/error'))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
