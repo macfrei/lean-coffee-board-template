2df8514503f1d28b630234c3dd5615c4353a3475
Marie-Carleen Frei <lene@7freiheiten.de>
2021-09-26 12:53:14 +0200

    Enhance structure

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.get('/api/cards', (req, res) => {
 app.post('/api/cards', (req, res) => {
   const id = nanoid()
   const newCard = { ...req.body, id }
-
   cards.push(newCard)
+
   res.status(200).json(newCard)
 })
 
@@ -24,15 +24,15 @@ app.patch('/api/cards/:id', (req, res) => {
   const index = cards.findIndex(card => id === card.id)
 
   const newCard = { ...cards[index], ...req.body }
-
   cards = [...cards.slice(0, index), newCard, ...cards.slice(index + 1)]
+
   res.status(200).send(newCard)
 })
 
 app.delete('/api/cards/:id', (req, res) => {
   const { id } = req.params
-
   cards = cards.filter(card => card.id !== id)
+
   res.status(200).send(`Card with id: ${id} was deleted.`)
 })
 
